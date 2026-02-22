3415ea9382cf6a8e7efed783ce5ec5559b579cae
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-02-22 20:08:11 +0900

    디자인 수정

--- src/components/BigCategoryLabel.jsx
+++ src/components/BigCategoryLabel.jsx
@@ -2,17 +2,11 @@ export default function BigCategoryLabel({ category, onClick }) {
   return (
     <div className="bigCategoryWrap">
       <button
-        className="bigCategoryLabel menuBox"
+        className="bigCategoryLabel"
         onClick={onClick ? () => onClick(category) : undefined}
         style={{ cursor: onClick ? "pointer" : "default" }}
       >
-        <span className="menuBoxCorner menuBoxCorner--tl" />
-        <span className="menuBoxCorner menuBoxCorner--tr" />
-        <span className="menuBoxCorner menuBoxCorner--bl" />
-        <span className="menuBoxCorner menuBoxCorner--br" />
-        <span style={{ position: "relative", zIndex: 1 }}>
-          {category ? category.toUpperCase() : ""}
-        </span>
+        {category ? category.toUpperCase() : ""}
       </button>
     </div>
   );
