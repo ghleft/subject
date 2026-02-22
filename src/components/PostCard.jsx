4250f838c7227928d2f5e582e2aa28b41a44eed7
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-02-22 17:24:16 +0900

    리스트 디자인 수정

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,8 +1,8 @@
 export default function PostCard({ post, onClick }) {
   return (
     <div className="list-item" onClick={onClick} role="button" tabIndex={0}>
+      {post.tag && <span className="item-tag">{post.tag}</span>}
       <div className="item-left">
-        {post.tag && <span className="item-tag">{post.tag}</span>}
         <div className="item-title">{post.title}</div>
       </div>
       <div className="item-author">{post.author}</div>
